3d224cee79affa323a47c4fbab009a85960d39e7
吴东 <wudong@eastwu.cn>
2020-06-28 10:49:10 +0800

    add part of  funcs support for redis client

--- flask_redis/redis_ext.py
+++ flask_redis/redis_ext.py
@@ -930,3 +930,170 @@ class RedisExtension:
         """
         # TODO: Update keys
         return self.redis.xtrim(name, maxlen, approximate)
+
+    def zadd(self, name, mapping, nx=False, xx=False, ch=False, incr=False):
+        """
+        Set any number of element-name, score pairs to the key ``name``. Pairs
+        are specified as a dict of element-names keys to score values.
+
+        ``nx`` forces ZADD to only create new elements and not to update
+        scores for elements that already exist.
+
+        ``xx`` forces ZADD to only update scores of elements that already
+        exist. New elements will not be added.
+
+        ``ch`` modifies the return value to be the numbers of elements changed.
+        Changed elements include new elements that were added and elements
+        whose scores changed.
+
+        ``incr`` modifies ZADD to behave like ZINCRBY. In this mode only a
+        single element/score pair can be specified and the score is the amount
+        the existing score will be incremented by. When using this mode the
+        return value of ZADD will be the new score of the element.
+
+        The return value of ZADD varies based on the mode specified. With no
+        options, ZADD returns the number of new elements added to the sorted
+        set.
+        """
+        # TODO: Update keys
+        return self.redis.zadd(self._get_key(name), mapping, nx, xx, ch, incr)
+
+    def zcard(self, name):
+        "Return the number of elements in the sorted set ``name``"
+        return self.redis.zcard(self._get_key(name))
+
+    def zcount(self, name, min, max):
+        """
+        Returns the number of elements in the sorted set at key ``name`` with
+        a score between ``min`` and ``max``.
+        """
+        return self.redis.zcount(self._get_key(name), min, max)
+
+    def zincrby(self, name, amount, value):
+        "Increment the score of ``value`` in sorted set ``name`` by ``amount``"
+        return self.redis.zincrby(self._get_key(name), amount, value)
+
+    def zinterstore(self, dest, keys, aggregate=None):
+        """
+        Intersect multiple sorted sets specified by ``keys`` into
+        a new sorted set, ``dest``. Scores in the destination will be
+        aggregated based on the ``aggregate``, or SUM if none is provided.
+        """
+        # TODO: Update keys
+        return self.redis.zinterstore(dest, keys, aggregate)
+
+    def zlexcount(self, name, min, max):
+        """
+        Return the number of items in the sorted set ``name`` between the
+        lexicographical range ``min`` and ``max``.
+        """
+        return self.redis.zlexcount(self._get_key(name), min, max)
+
+    def zpopmax(self, name, count=None):
+        """
+        Remove and return up to ``count`` members with the highest scores
+        from the sorted set ``name``.
+        """
+        return self.redis.zpopmax(self._get_key(name), count)
+
+    def zpopmin(self, name, count=None):
+        """
+        Remove and return up to ``count`` members with the lowest scores
+        from the sorted set ``name``.
+        """
+        return self.redis.zpopmin(self._get_key(name), count)
+
+    def bzpopmax(self, keys, timeout=0):
+        """
+        ZPOPMAX a value off of the first non-empty sorted set
+        named in the ``keys`` list.
+
+        If none of the sorted sets in ``keys`` has a value to ZPOPMAX,
+        then block for ``timeout`` seconds, or until a member gets added
+        to one of the sorted sets.
+
+        If timeout is 0, then block indefinitely.
+        """
+        # TODO: Update keys
+        return self.redis.bzpopmax(keys, timeout)
+
+    def bzpopmin(self, keys, timeout=0):
+        """
+        ZPOPMIN a value off of the first non-empty sorted set
+        named in the ``keys`` list.
+
+        If none of the sorted sets in ``keys`` has a value to ZPOPMIN,
+        then block for ``timeout`` seconds, or until a member gets added
+        to one of the sorted sets.
+
+        If timeout is 0, then block indefinitely.
+        """
+        # TODO: Update keys
+        return self.redis.bzpopmin(keys, timeout)
+
+    def zrange(self, name, start, end, desc=False, withscores=False,
+               score_cast_func=float):
+        """
+        Return a range of values from sorted set ``name`` between
+        ``start`` and ``end`` sorted in ascending order.
+
+        ``start`` and ``end`` can be negative, indicating the end of the range.
+
+        ``desc`` a boolean indicating whether to sort the results descendingly
+
+        ``withscores`` indicates to return the scores along with the values.
+        The return type is a list of (value, score) pairs
+
+        ``score_cast_func`` a callable used to cast the score return value
+        """
+        # TODO: Update keys
+        return self.redis.zrange(name, start, end, desc, withscores, score_cast_func)
+
+    def zrangebylex(self, name, min, max, start=None, num=None):
+        """
+        Return the lexicographical range of values from sorted set ``name``
+        between ``min`` and ``max``.
+
+        If ``start`` and ``num`` are specified, then return a slice of the
+        range.
+        """
+        # TODO: Update keys
+        return self.redis.zrangebylex(name, min, max, start, num)
+
+    def zrevrangebylex(self, name, max, min, start=None, num=None):
+        """
+        Return the reversed lexicographical range of values from sorted set
+        ``name`` between ``max`` and ``min``.
+
+        If ``start`` and ``num`` are specified, then return a slice of the
+        range.
+        """
+        # TODO: Update keys
+        return self.redis.zrevrangebylex(name, max, min, start, num)
+
+    def zrangebyscore(self, name, min, max, start=None, num=None,
+                      withscores=False, score_cast_func=float):
+        """
+        Return a range of values from the sorted set ``name`` with scores
+        between ``min`` and ``max``.
+
+        If ``start`` and ``num`` are specified, then return a slice
+        of the range.
+
+        ``withscores`` indicates to return the scores along with the values.
+        The return type is a list of (value, score) pairs
+
+        `score_cast_func`` a callable used to cast the score return value
+        """
+        return self.redis.zrangebyscore(self._get_key(name), min, max, start, num, withscores, score_cast_func)
+
+    def zrank(self, name, value):
+        """
+        Returns a 0-based value indicating the rank of ``value`` in sorted set
+        ``name``
+        """
+        return self.redis.zrank(self._get_key(name), value)
+
+    def zrem(self, name, *values):
+        "Remove member ``values`` from sorted set ``name``"
+        return self.redis.zrem(self._get_key(name), *values)
