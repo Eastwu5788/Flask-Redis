ba7d06eb744e74dcdb1bb1e94082883520d20bc5
吴东 <wudong@eastwu.cn>
2021-12-28 16:53:17 +0800

    system reconstruction

--- flask_redis/macro.py
+++ flask_redis/macro.py
@@ -5,14 +5,13 @@
 # @Author: 'Wu Dong <wudong@eastwu.cn>'
 # @Time: '2020-06-24 16:10'
 
-# redis key prefix
-K_RDS_PREFIX = "REDIS_PREFIX"
-
 # redis binds
 K_RDS_BINDS = "REDIS_BINDS"
-
+# redis prefix
+K_RDS_PREFIX = "REDIS_PREFIX"
 # redis url
 K_RDS_URL = "REDIS_URL"
 
-# redis default key
-K_RDS_DEFAULT_BIND = "REDIS_DEFAULT_BIND"
+# ignore cache
+K_RDS_IGNORE_CACHE = "REDIS_IGNORE_CACHE"
+K_INTERNAL_IGNORE_CACHE = "IGNORE_CACHE"
